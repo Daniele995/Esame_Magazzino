9e067a917b11315388cb38db7acdf28783bb69d1
Daniele995 <scuolap488@gmail.com>
2026-08-21 18:03:17 +0200

    Creazione package controller 1

---
+++ src/main/java/Controller/ControllerAutenticazione.java
@@ -0,0 +1,29 @@
+package Controller;
+
+public class ControllerAutenticazione {
+
+    public static final int EMAIL_GIA_ESISTENTE = 0;
+    public static final int REGISTRAZIONE_SUCCESSO = 1;
+    public static final int ERRORE_DATI = -1;
+    public static final int ACCESSO_SUCCESSO = 2;
+
+
+    public int registrazioneUtente(String nome, String cognome, String email, String ruolo) {
+        //Controllo campi
+        if (nome == null || nome.isEmpty() || cognome == null || cognome.isEmpty() || email == null || email.isEmpty() || ruolo == null || ruolo.isEmpty()) {
+            return ERRORE_DATI;
+        }
+
+        return REGISTRAZIONE_SUCCESSO;
+    }
+
+
+    public int accesso(String email, String ruolo) {
+        //Controllo campi
+        if (email == null || email.isEmpty() || ruolo == null || ruolo.isEmpty()) {
+            return ERRORE_DATI;
+        }
+
+        return ERRORE_DATI;
+    }
+}
