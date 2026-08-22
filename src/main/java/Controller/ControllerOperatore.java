8e5198c4239edb4b4c0c822debf12984608a7c5d
Daniele995 <scuolap488@gmail.com>
2026-08-22 10:52:15 +0200

    PController

--- src/main/java/Controller/ControllerOperatore.java
+++ src/main/java/Controller/ControllerOperatore.java
@@ -1,4 +1,8 @@
 package Controller;
 
 public class ControllerOperatore {
+    public static final int MOVIMENTO_NON_REGISTRATO=0;
+    public static final int MOVIMENTO_REGISTRATO_SUCCESSO=1;
+    public static final int ERRORE_QUANTITA_INSUFFICIENTE=-1;
+
 }
--- src/main/java/Controller/ControllerResponsabile.java
+++ src/main/java/Controller/ControllerResponsabile.java
@@ -1,4 +1,9 @@
 package Controller;
 
 public class ControllerResponsabile {
+    public static final int PRODOTTO_NON_INSERITO=0;
+    public static final int PRODOTTO_INSERITO_SUCCESSO=1;
+    public static final int PRODOTTO_MODIFICATO_SUCCESSO=2;
+    public static final int ERRORE_DATI_PRODOTTO=-1;
+
 }
