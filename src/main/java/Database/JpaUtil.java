93b780ea5fd055efa988efcd3938476584a59cf2
Emanuele Grasso <emanuele.grasso9@gmail.com>
2026-08-23 15:31:59 +0200

    GestorePersistenza e JpaUtil

---
+++ src/main/java/Database/JpaUtil.java
@@ -0,0 +1,27 @@
+package Database;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaUtil {
+
+    public static JpaUtil instance;
+
+    private EntityManagerFactory emf;
+
+    private JpaUtil(){
+        emf = Persistence.createEntityManagerFactory("MagazzinoPU");
+    }
+
+    public static JpaUtil getInstance(){
+        if (instance == null){
+            instance = new JpaUtil();
+        }
+        return instance;
+    }
+
+    public EntityManager getEntityManager(){ return emf.createEntityManager();}
+
+    public void chiudi(){emf.close();}
+}
